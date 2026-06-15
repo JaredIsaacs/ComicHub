0e744615109ac57ccd6b1803cec12adf4550b1cd
JaredIsaacs <jaredisaacs6@gmail.com>
2026-06-15 11:40:26 -0500

    scraper pylint fixes

--- src/scrapers/scraper.py
+++ src/scrapers/scraper.py
@@ -22,6 +22,18 @@ class Scraper(ABC):
 
     @staticmethod
     def get_scraper(class_name: str, base_url: str) -> Scraper:
+        """Static method used to dynamically allow the retrieval of a scraper class that
+        derives from the base Scraper class.
+        
+        Requires:
+            * class_name: str - this needs to be the name of the class under the scrapers folder, 
+            as well as the name of the class in the config.json file. After an initial scrape this
+            field can also be found in the database under the Source table.
+            * base_url: str - the base_url of a source site. Can be found in the config file.
+            
+        Returns:
+            * Scraper - the requested for class based on the Scraper class."""
+
         module = importlib.import_module(f"src.scrapers.{class_name}")
         scraper_class = getattr(module, class_name)
 
@@ -30,8 +42,23 @@ class Scraper(ABC):
 
     @abstractmethod
     def get_all_comics(self) -> list[Comic]:
-        pass
-    
+        """Method used that returns a list of all comics from a selected source.
+        
+        Requires:
+            * Nothing
+
+        Returns:
+            * list[Comic]
+        """
+
+
     @abstractmethod
     def get_comic(self, slug: str) -> Comic:
-        pass
+        """Method that returns a specific comic provided a slug/id/name.
+        
+        Requires:
+            * slug/id/name: str - This can be any of the speicified fields.
+
+        Returns:
+            Comic
+        """
