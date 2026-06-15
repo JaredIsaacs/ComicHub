b51a4b6021adf151e599f532ff946b5eb4f70bd7
JaredIsaacs <jaredisaacs6@gmail.com>
2026-06-15 15:48:16 -0500

    chore: removed redundant .keys() method call on line 65.

--- src/scrapers/scraper.py
+++ src/scrapers/scraper.py
@@ -11,7 +11,7 @@ from src.scrapers.comic import Comic
 
 class Scraper(ABC):
     """Base class for all scrapers.
-    
+
     If this is being used to scrape, something went majorly wrong.
     """
 
@@ -23,17 +23,17 @@ class Scraper(ABC):
         self.name = "Base Scraper"
         self.session = Session()
 
-    
+
     def __init_subclass__(cls):
         super().__init_subclass__()
 
         Scraper._class_registry[cls.__name__] = cls
 
-    
+
     @staticmethod
     def initialize_registry():
         """Static method used to initialize scrapers into the base classes registry.
-        
+
         Call this method before get_scraper()
         """
 
@@ -52,17 +52,17 @@ class Scraper(ABC):
     def get_scraper(class_name: str, base_url: str) -> Scraper:
         """Static method used to dynamically allow the retrieval of a scraper class that
         derives from the base Scraper class.
-        
+
         Requires:
-            * class_name: str - this needs to be the name of the class under the scrapers folder, 
+            * class_name: str - this needs to be the name of the class under the scrapers folder,
             as well as the name of the class in the config.json file. After an initial scrape this
             field can also be found in the database under the Source table.
             * base_url: str - the base_url of a source site. Can be found in the config file.
-            
+
         Returns:
             * Scraper - the requested for class based on the Scraper class."""
 
-        if class_name not in Scraper._class_registry.keys():
+        if class_name not in Scraper._class_registry:
             raise ValueError(f"Class name, {class_name}, not in class registry.")
 
         return Scraper._class_registry[class_name](base_url)
@@ -71,7 +71,7 @@ class Scraper(ABC):
     @abstractmethod
     def get_all_comics(self) -> list[Comic]:
         """Method used that returns a list of all comics from a selected source.
-        
+
         Requires:
             * Nothing
 
@@ -83,7 +83,7 @@ class Scraper(ABC):
     @abstractmethod
     def get_comic(self, slug: str) -> Comic:
         """Method that returns a specific comic provided a slug/id/name.
-        
+
         Requires:
             * slug/id/name: str - This can be any of the speicified fields.
 
